696f24c58bbb84ded82710515603a39dc95b3027
Haotian.Chang <cht8687@gmail.com>
2016-03-15 21:24:29 +1100

    Add in React-Redux, configure with empty store

--- Learn-by-doing-mocking-Gmail-UI-React-Redux/src/containers/Root.js
+++ Learn-by-doing-mocking-Gmail-UI-React-Redux/src/containers/Root.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import configureStore from '../store/configureStore';
+
+const store = configureStore();
 
 export default class Root extends Component {
 
   render() {
 
     return (
-      <div> hey ya... </div>
+      <Provider store={store}>
+        <div> hey ya... </div>
+      </Provider>
     );
   }
 }
