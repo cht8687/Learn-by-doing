e8aab6061d43c1bc843a5eab3d4fffde55606a3a
Haotian.Chang <cht8687@gmail.com>
2016-03-22 21:30:18 +1100

    handle click

--- Learn-by-doing-mocking-Gmail-UI-React-Redux/src/components/TopBar/SearchBar/Dropdown.js
+++ Learn-by-doing-mocking-Gmail-UI-React-Redux/src/components/TopBar/SearchBar/Dropdown.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import styles from 'styles/Topbar/topbar.styl';
 
+
 export default class DropDown extends React.Component {
 
   render() {
     return (
-      <div className={styles.dropDown}>
+      <div 
+      	className={styles.dropDown}
+      	onClick={this.handleDropDownClick.bind(this)}
+      >
       </div>
     );
+  }
+  
+  handleDropDownClick() {
+  	// I need the dispatcher here....
+  	// I am going to connect the dispatcher tommorrow
+  	
   }
 }
